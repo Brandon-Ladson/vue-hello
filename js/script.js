7433c757af76e70dd55458a5405da4fe612b27e1
Brandon Ladson <brandonladson1999@gmail.com>
2020-11-16 15:17:29 +0100

    creazione regole css e js e aggiunta titolo e immagine con vue

--- js/script.js
+++ js/script.js
@@ -1,2 +1,21 @@
 // Stampare a schermo un messaggio all’interno di un h1, utilizzando i data.
 // Bonus: Aggiungere un’immagine presa anch’essa da un data
+// Altro possibile Bonus:
+// gestire una funzione con un evento nel modo Vue.
+var root = new Vue({
+  el: '#root',
+  data: {
+    message: 'Cane Lupo Cecoslovacco',
+    myClass: 'invisibile',
+    immagine: "https://www.lifegate.it/app/uploads/primo-piano-faccia-clc-e1548079110876.jpg"
+  },
+  methods: {
+    mostra: function () {
+      if (this.myClass === 'invisibile') {
+        this.myClass = 'visibile'
+      } else {
+        this.myClass = 'invisibile'
+      }
+    }
+  }
+});
